45b7a32123749f83ece1824c1e893c29cc9a9398
Marlena R Macamo <marlenemacamo22@outlook.com>
2023-08-18 01:46:44 -0700

    Printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing numbers
+ *
+ * Return: 0 (Always)
+ */
+
+int main(void)
+
+{
+	int n;
+	int i = 10;
+
+	for (n = '0' ; n <= i ; n++)
+		printf("Single digit %d", n);
+	printf("\n");
+
+	return (0);
+}
