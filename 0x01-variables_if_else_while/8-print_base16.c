1ba0317797abcaf249c0cc0a76f9dbf207963479
Marlena R Macamo <marlenemacamo22@outlook.com>
2023-08-18 03:32:27 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 (always)
+ */
+
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 0 ; n < 10 ; n++)
+		putchar(n + '0');
+	for (c = 'a' ; c <= 'f' ; c++)
+		putchar(c);
+	return (0);
+}
