6134e00345d26f9d942ad6984e3ad9384dad1cf7
Marlena R Macamo <marlenemacamo22@outlook.com>
2023-08-21 17:25:15 -0700

    changing the value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98  - reset n to 98
+ * @n: pointer of a value that needs to be changed
+ * Return: 0 (Always)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
