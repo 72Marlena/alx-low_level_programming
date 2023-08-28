fded8fef035e38c60409dd48e4ee4871e2143884
Marlena R Macamo <marlenemacamo22@outlook.com>
2023-08-28 16:34:49 -0700

    Two digits ranging from zero to nightynine

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ *main - Prints all possible combinations of two two-digit numbers,
+ *        ranging from 0-9.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n < 100; n++)
+	{
+		for (m = 0; m < 100; m++)
+		{
+			if (n < m)
+			{
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
+				putchar(' ');
+				putchar((m / 10) + 48);
+				putchar((m % 10) + 48);
+				if (n != 98 || m != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
