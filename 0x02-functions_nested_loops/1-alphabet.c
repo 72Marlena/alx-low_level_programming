2a345c5725013957c39d65101020641e8dee4a10
Marlena R Macamo <marlenemacamo22@outlook.com>
2023-09-05 06:12:07 -0700

    printing alphabets in lower cases, So Fun

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printss alphabets in lower cases
+ *
+ * Return: 0 always
+ */
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+		_putchar(n);
+	{
+		_putchar('\n');
+	}
+}
