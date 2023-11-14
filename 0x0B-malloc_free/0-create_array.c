fc519b99c56b15c70fc439ffcf36e878ee7a58e0
Marlena Macamo <marlenemacamo22@outlook.com>
2023-11-14 04:14:51 -0800

    html

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,26 +1,6 @@
-#include "main.h"
-
-/**
- * create_array - creates an array of chars
- * @size: size of an array
- * @c: the character tyo be entered
- * Return: ptr
- */
-
-char *create_array(unsigned int size, char c)
-{
-	unsigned int j;
-	char *ptr;
-
-	if (size == 0)
-		return (NULL);
-
-	ptr = malloc(size * sizeof(char));
-
-	if (ptr == NULL)
-		return (NULL);
-
-	for (j = 0 ; j < size ; j++)
-		ptr[j] = c;
-	return (ptr);
-}
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unstd.h>
+#include <stdlib.h>
+#include <string.h>
